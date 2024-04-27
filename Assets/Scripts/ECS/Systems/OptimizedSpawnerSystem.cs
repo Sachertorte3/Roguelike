653462526a4c2bf15aba9249287a05048ce14e48
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-04-27 15:39:01 +0900

    add packages

--- Assets/Scripts/ECS/Systems/OptimizedSpawnerSystem.cs
+++ Assets/Scripts/ECS/Systems/OptimizedSpawnerSystem.cs
@@ -1,57 +1,28 @@
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Transforms;
 using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
-[BurstCompile]
-public partial struct OptimizedSpawnerSystem : ISystem
+public partial struct SpriteGenerator : ISystem
 {
-    public void OnCreate(ref SystemState state) { }
-
-    public void OnDestroy(ref SystemState state) { }
-
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
+    public void OnCreate(ref SystemState state)
     {
-        EntityCommandBuffer.ParallelWriter ecb = GetEntityCommandBuffer(ref state);
-
-        // ジョブの新しいインスタンスを作成し、必要なデータを割り当て、ジョブを並列してスケジュールします。
-        new ProcessSpawnerJob
-        {
-            ElapsedTime = SystemAPI.Time.ElapsedTime,
-            Ecb = ecb
-        }.ScheduleParallel();
+        // Spawnerの全Componentを問い合わせる。
+        // このSystemでは、Componentへの読み取りと書き込みを行いたいので、RefRWを使用する。
+        // Systemが読み取り専用のみを要するなら、RefROを使用する。
+        ProcessSpawner(ref state);
     }
 
-    private EntityCommandBuffer.ParallelWriter GetEntityCommandBuffer(ref SystemState state)
+    private void ProcessSpawner(ref SystemState state)
     {
-        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
-        return ecb.AsParallelWriter();
-    }
-}
-
-[BurstCompile]
-public partial struct ProcessSpawnerJob : IJobEntity
-{
-    public EntityCommandBuffer.ParallelWriter Ecb;
-    public double ElapsedTime;
-
-    // IJobEntityは、その`Execute`メソッドのパラメータに基づいて、コンポーネントデータクエリを生成します。
-    // この例では、すべてのSpawnerコンポーネントをクエリし、`ref`を使用して、操作に読み取りと書き込みのアクセスが必要であることを指定します。
-    // Unityは、コンポーネントデータクエリに一致する各エンティティに対して`Execute`を処理します。
-
-    private void Execute([ChunkIndexInQuery] int chunkIndex, ref Spawner spawner)
-    {
-        // 次のスポーン時間が経過している場合
-        if (spawner.NextSpawnTime < ElapsedTime)
-        {
-            // 新しいエンティティを生成し、スポナーに配置します。
-            Entity newEntity = Ecb.Instantiate(chunkIndex, spawner.Prefab);
-            Ecb.SetComponent(chunkIndex, newEntity, LocalTransform.FromPosition(spawner.SpawnPosition));
-
-            // 次のスポーン時間をリセットします。
-            spawner.NextSpawnTime = (float)ElapsedTime + spawner.SpawnRate;
-        }
+        // Spawnerの位置に新しいEntityを生成する。
+        Entity newEntity = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponent(newEntity, typeof(Character));
+        state.EntityManager.AddComponent(newEntity, typeof(LocalTransform));
+        state.EntityManager.AddComponent(newEntity, typeof(SpriteRenderer));
     }
 }

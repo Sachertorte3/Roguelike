4b60ef478b769facbc97ec0bfb947c2098ac8414
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-04-28 18:55:17 +0900

    feat: Add character manager

--- Assets/Scripts/Provider/Spawner.cs
+++ Assets/Scripts/Provider/Spawner.cs
@@ -4,21 +4,26 @@ using Scripts.View;
 using UniRx;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.PlayerLoop;
 
 public class Spawner: MonoBehaviour
 {
     [SerializeField] InputReceiver receiver;
     public void Start()
     {
-        GameObject prefab = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/CharacterView.prefab").WaitForCompletion();
-        GameObject view = Instantiate(prefab);
-        Character character = new Character();
-        character.MoveSubject.Subscribe(direction => view.GetComponent<CharacterView>().Move(direction));
+        CharacterManager characterManager = new CharacterManager();
+        characterManager.Characters.ObserveAdd().Subscribe(character =>
+        {
+            GameObject prefab = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/CharacterView.prefab").WaitForCompletion();
+            GameObject view = Instantiate(prefab);
+            character.Value.MoveSubject.Subscribe(direction => view.GetComponent<CharacterView>().Move(direction));
+        }).AddTo(this);
+        Character player = characterManager.SpawnPlayer();
         receiver.MoveDirection
             .Where(x => x != Vector2.zero)
             .Subscribe(x => {
                 Direction8 direction = DirectionMethods.FromVector(x);
-                character.Move(direction);
+                player.Move(direction);
             })
             .AddTo(this);
     }

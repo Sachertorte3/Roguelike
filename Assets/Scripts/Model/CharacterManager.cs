ff69fe59413bd4635a6cb569a98f6c9f49ad2b62
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-04-28 18:55:17 +0900

    feat: Add character manager

---
+++ Assets/Scripts/Model/CharacterManager.cs
@@ -0,0 +1,30 @@
+﻿using UniRx;
+
+namespace Scripts.Model
+{
+    public sealed class CharacterManager
+    {
+        public Character? Player;
+        private Character? _player = null;
+        private ReactiveCollection<Character> _characters = new ReactiveCollection<Character>();
+        public IReadOnlyReactiveCollection<Character> Characters => _characters;
+        private readonly CharacterFactory _factory = new CharacterFactory();
+        public Character SpawnPlayer()
+        {
+            _player = _factory.CreateCharacter();
+            _characters.Add(_player);
+            return _player;
+        }
+        public void SpawnCharacter()
+        {
+            _characters.Add(_factory.CreateCharacter());
+        }
+    }
+    internal sealed class CharacterFactory
+    {
+        public Character CreateCharacter()
+        {
+            return new Character();
+        }
+    }
+}

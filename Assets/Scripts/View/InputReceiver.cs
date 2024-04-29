8fb014356979481a5efd85f63a208fa6247dd855
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-04-30 03:45:07 +0900

    feat: Modify to read input only when necessary

--- Assets/Scripts/View/InputReceiver.cs
+++ Assets/Scripts/View/InputReceiver.cs
@@ -1,4 +1,5 @@
 using Scripts.Utilities;
+using System;
 using UniRx;
 using UnityEngine;
 
@@ -7,13 +8,12 @@ namespace Scripts.View
     public class InputReceiver : MonoBehaviour
     {
         private MyInputAction _actions;
-        public Vector2 MoveDirection => _actions.Field.Move.ReadValue<Vector2>();
-        //public ReadOnlyReactiveProperty<Vector2> MoveDirection => _actions.Field.Move.AsReactiveProperty<Vector2>();
+        public IObservable<Vector2> OnMovePerformed => _actions.Field.Move.AsObservable().Select(context => context.ReadValue<Vector2>());
+        public Vector2 MoveVector => _actions.Field.Move.ReadValue<Vector2>();
         private void Start()
         {
             _actions = new MyInputAction();
             _actions.Enable();
-            //MoveDirection.AddTo(this);
         }
     }
 }

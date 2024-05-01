f398b5afc1b451cd0d55ff0b0bd6f23f7f4ea3af
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-02 02:55:28 +0900

    feat: import VContainer

---
+++ Assets/Scripts/Provider/Container.cs
@@ -0,0 +1,27 @@
+﻿using RandomDungeonWithBluePrint;
+using Scripts.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Scripts.Provider
+{
+    internal class Container: LifetimeScope
+    {
+        [SerializeField] private FieldBluePrint _bluePrint;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<InputReceiver>(Lifetime.Singleton);
+            builder.RegisterComponentInHierarchy<TileViewContriller>();
+            builder.RegisterComponent(_bluePrint);
+            builder.RegisterComponentInHierarchy<CameraFollowTarget>();
+            builder.RegisterEntryPoint<Presenter>(Lifetime.Scoped);
+        }
+    }
+}
---
+++ Assets/Scripts/Provider/Presenter.cs
@@ -0,0 +1,105 @@
+#nullable enable
+using RandomDungeonWithBluePrint;
+using Scripts.Model;
+using Scripts.Model.Characters;
+using Scripts.Model.Characters.Behavior;
+using Scripts.Model.Map;
+using Scripts.Utilities;
+using Scripts.View;
+using System.Collections.Generic;
+using UI;
+using UniRx;
+using Unity.Logging;
+using Unity.Logging.Sinks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using VContainer.Unity;
+using Logger = Unity.Logging.Logger;
+
+namespace Scripts.Provider
+{
+    public class Presenter : IPostInitializable
+    {
+        private readonly InputReceiver receiver;
+        private readonly TileViewContriller tileView;
+        private readonly FieldBluePrint bluePrint;
+        private readonly CameraFollowTarget _camera;
+        private Dictionary<Character, CharacterView> characterViewDict = new Dictionary<Character, CharacterView>();
+        public Presenter(InputReceiver receiver, TileViewContriller tileView, FieldBluePrint bluePrint, CameraFollowTarget camera)
+        {
+            this.receiver = receiver;
+            this.tileView = tileView;
+            this.bluePrint = bluePrint;
+            this._camera = camera;
+        }
+        public void PostInitialize()
+        {
+            LoggerInit();
+
+            Debug.Log(receiver);
+
+            Tilemap map = new Tilemap(bluePrint);
+            map.OnChangeTile.Subscribe(context =>
+            {
+                switch (context.tile.TileType)
+                {
+                    case TileCategory.Wall:
+                        tileView.SetWall(context.position);
+                        break;
+                    case TileCategory.Floor:
+                        tileView.SetFloor(context.position);
+                        break;
+                }
+            });
+            foreach ((Vector2Int position, TileData tileData) in map.GetAllTiles())
+            {
+                switch (tileData.TileType)
+                {
+                    case TileCategory.Wall:
+                        tileView.SetWall(position);
+                        break;
+                    case TileCategory.Floor:
+                        tileView.SetFloor(position);
+                        break;
+                }
+            }
+
+            CharacterManager characterManager = new CharacterManager();
+
+            World world = new World(map, characterManager);
+
+            characterManager.Characters.ObserveAdd().Subscribe(character =>
+            {
+                GameObject prefab = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/CharacterView.prefab").WaitForCompletion();
+                CharacterView view = Object.Instantiate(prefab).GetComponent<CharacterView>();
+                view.transform.position = (Vector3Int)character.Value.Position.Value;
+                character.Value.OnMove.Subscribe(direction => view.OnMove.OnNext((character.Value.Position.Value, direction))).AddTo(view);
+                characterViewDict[character.Value] = view;
+            });
+            ActionReceiver actionReceiver = new ActionReceiver();
+            Observable.Merge(
+                receiver.OnMovePerformed,
+                actionReceiver.OnWait.Select(_ => receiver.MoveVector)
+            )
+                .Where(vector => vector != Vector2.zero)
+                .Subscribe(vector =>
+                {
+                    Direction8 direction = DirectionMethods.FromVector(vector);
+                    actionReceiver.SetMoveAction(direction);
+                });
+            characterManager.SpawnPlayer(map.GetAllPassablePositions().GetAtRandom(), actionReceiver, world);
+            characterManager.SpawnCharacter(map.GetAllPassablePositions().GetAtRandom(), world);
+            _camera.SetTarget(characterViewDict[characterManager.Player].gameObject);
+
+            new TurnController(characterManager);
+        }
+        private void LoggerInit()
+        {
+            Log.Logger = new Logger(new LoggerConfig()
+                .MinimumLevel.Debug()
+                .OutputTemplate("[{Timestamp}] {Level} | {Message}{NewLine}{Stacktrace}")
+                .WriteTo.UnityDebugLog());
+            Log.Debug("Init Logger");
+        }
+    }
+}

34a142e08f55a912343d6b6c9c007d0abc08dcd8
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-02 14:57:01 +0900

    feat: Implement a function to set Setting items from a script

---
+++ Assets/Scripts/View/UI/SettingWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Scripts.View.UI
+{
+    internal class SettingWindow: MonoBehaviour
+    {
+        [SerializeField] Transform _content;
+        [SerializeField] SliderItem _sliderItem;
+        private void Start()
+        {
+            AddValueItem("BGM音量(ダミー)", 0, 100, 50);
+            AddValueItem("SE音量(ダミー)", 0, 100, 50);
+        }
+        public void AddValueItem(string itemName, int min, int max, int value)
+        {
+            _sliderItem.SetData(itemName, min, max, value);
+            GameObject.Instantiate(_sliderItem, _content);
+        }
+    }
+}
---
+++ Assets/Scripts/View/UI/SliderItem.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.View.UI
+{
+    internal class SliderItem: MonoBehaviour
+    {
+        [SerializeField] TMP_Text _text;
+        [SerializeField] Slider _slider;
+        public void SetData(string itemName, int min, int max, int value)
+        {
+            _text.SetText(itemName);
+            _slider.minValue = min;
+            _slider.maxValue = max;
+            _slider.value = value;
+        }
+    }
+}

8933de3e4aaace70bd6172d13c67483e55b2509b
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-02 14:28:12 +0900

    feat: Create a dummy Setting screen

---
+++ Assets/Scripts/View/UI/SliderText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UniRx;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.View.UI
+{
+    internal class SliderText: MonoBehaviour
+    {
+        [SerializeField] Slider _slider;
+        [SerializeField] TMP_Text _text;
+        private void Start()
+        {
+            _text.SetText(_slider.value.ToString());
+            _slider.onValueChanged.AsObservable().Subscribe(value =>
+            {
+                _text.SetText(value.ToString());
+            });
+        }
+    }
+}

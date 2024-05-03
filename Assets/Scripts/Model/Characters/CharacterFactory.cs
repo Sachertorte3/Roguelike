580bc88facc4e17d825cdd66b39110006e96e984
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-03 13:05:43 +0900

    feat: Change wall ignoring to apply only to Player

--- Assets/Scripts/Model/Characters/CharacterFactory.cs
+++ Assets/Scripts/Model/Characters/CharacterFactory.cs
@@ -1,14 +1,15 @@
 ﻿#nullable enable
 using Scripts.Model.Characters.Behavior;
+using UniRx;
 using UnityEngine;
 
 namespace Scripts.Model.Characters
 {
     internal sealed class CharacterFactory
     {
-        public Character CreateCharacter(Vector2Int spawnPosition, ICharacterBehavior behavior, World world)
+        public Character CreateCharacter(Vector2Int spawnPosition, ICharacterBehavior behavior, World world, ReactiveProperty<bool> canIgnoreWall)
         {
-            return new Character(spawnPosition, behavior, world);
+            return new Character(spawnPosition, behavior, world, canIgnoreWall);
         }
     }
 }

0ff931f9933553ded56bd165387be0074ab7c7eb
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-05 13:47:22 +0900

    feat: Adds the ability to pause momentarily when approaching a fork in the road while dashing.

--- Assets/Scripts/Model/Characters/Behavior/PlayerBehavior.cs
+++ Assets/Scripts/Model/Characters/Behavior/PlayerBehavior.cs
@@ -1,11 +1,14 @@
 ﻿using Cysharp.Threading.Tasks;
 using Scripts.Model.Action;
+using Scripts.Model.Setting;
+using Scripts.Utilities;
 
 namespace Scripts.Model.Characters.Behavior
 {
     internal sealed class PlayerBehavior : ICharacterBehavior
     {
         private ActionReceiver _actionReceiver;
+        private IntelligentDashController _intelligentDashController = new IntelligentDashController();
         public PlayerBehavior(ActionReceiver actionReceiver)
         {
             _actionReceiver = actionReceiver;
@@ -19,10 +22,30 @@ namespace Scripts.Model.Characters.Behavior
                 IAction action = await actionTask;
                 if (action.Doable(character))
                 {
-                    return action;
+                    return action switch
+                    {
+                        Move move => await _intelligentDashController.Filter(move, character),
+                        _ => action
+                    };
                 }
                 actionTask = _actionReceiver.ReceivedAction.WaitAsync();
             }
+        }
+    }
+    internal sealed class IntelligentDashController
+    {
+        public async UniTask<Move> Filter(Move move, IHasBehavior character)
+        {
+            if (character.CanMove(move.Direction.Reverse()) &&
+                (
+                    (character.CanMove(move.Direction.Rotate90Clockwise()) && !character.CanMove(move.Direction.Reverse().Rotate45AntiClockwise()))
+                    || (character.CanMove(move.Direction.Rotate90AntiClockwise()) && !character.CanMove(move.Direction.Reverse().Rotate45Clockwise()))
+                )
+            )
+            {
+                await UniTask.Delay(Settings.MoveMilliseconds.Value);
+            }
+            return move;
         }
     }
 }

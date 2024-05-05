8e614c416bd56cc179139daf7b27dec91a38bd40
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-05 17:31:37 +0900

    feat: Modify so that attacks can be performed in the direction the player is facing.

--- Assets/Scripts/Model/Characters/Behavior/PlayerBehavior.cs
+++ Assets/Scripts/Model/Characters/Behavior/PlayerBehavior.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 using Cysharp.Threading.Tasks;
+using R3;
 using Scripts.Model.Action;
+using Scripts.Model.Characters.Effect;
 using Scripts.Model.Setting;
 using Scripts.Utilities;
 using System;
@@ -12,11 +14,11 @@ namespace Scripts.Model.Characters.Behavior
 {
     internal sealed class PlayerBehavior : ICharacterBehavior
     {
-        private ActionReceiver _actionReceiver;
+        private CharacterControllInputReceiver _receiver;
         private IntelligentDashController _intelligentDashController = new IntelligentDashController();
-        public PlayerBehavior(ActionReceiver actionReceiver)
+        public PlayerBehavior(CharacterControllInputReceiver receiver)
         {
-            _actionReceiver = actionReceiver;
+            _receiver = receiver;
         }
         public bool IsDashingStraight(bool started, IAction action)
         {
@@ -25,32 +27,58 @@ namespace Scripts.Model.Characters.Behavior
         public async UniTask<IAction> GenerateNextAction(IHasBehavior character)
         {
             await _intelligentDashController.Wait(character);
-            UniTask<(IAction action, bool isStarted)> actionTask = _actionReceiver.ReceivedAction.WaitAsync();
-            _actionReceiver.ReadInput();
+
+            UniTask<(Move action, bool isStarted)> moveTask = _receiver.OnMoveInputReceived.WaitAsync();
+            UniTask<Skill> skillTask = _receiver.OnSkillActionReceived.WaitAsync();
+
+            _receiver.ReadInput();
+
+            var firstCompletedTask = await UniTask.WhenAny(moveTask, skillTask);
             while (true)
             {
-                (IAction action, bool started) = await actionTask;
-                if (action is Move move && GameManager.IsNoMove())
+                switch (firstCompletedTask.winArgumentIndex)
                 {
-                    character.Turn(move.Direction);
-                }
-                else
-                {
-                    if (Settings.IntelligentDash.Value && IsDashingStraight(started, action))
-                    {
-                        action = _intelligentDashController.Filter((Move)action, character);
-                    }
-                    else
-                    {
-                        _intelligentDashController.Reset();
-                    }
+                    case 0:
+                        (Move move, bool started) = firstCompletedTask.result1;
+                        if (GameManager.IsNoMove())
+                        {
+                            character.Turn(move.Direction);
+                        }
+                        else
+                        {
+                            if (Settings.IntelligentDash.Value && IsDashingStraight(started, move))
+                            {
+                                move = _intelligentDashController.Filter(move, character);
+                            }
+                            else
+                            {
+                                _intelligentDashController.Reset();
+                            }
 
-                    if (action.Doable(character))
-                    {
-                        return action;
-                    }
+                            if (move.Doable(character))
+                            {
+                                return move;
+                            }
+                        }
+                        break;
+                    case 1:
+                        Skill skill = firstCompletedTask.result2;
+                        IAction action = new UseSkill(skill, character.CurrentDirection);
+
+                        _intelligentDashController.Reset();
+
+                        if (action.Doable(character))
+                        {
+                            return action;
+                        }
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException();
                 }
-                actionTask = _actionReceiver.ReceivedAction.WaitAsync();
+
+                moveTask = _receiver.OnMoveInputReceived.WaitAsync();
+                skillTask = _receiver.OnSkillActionReceived.WaitAsync();
+                firstCompletedTask = await UniTask.WhenAny(moveTask, skillTask);
             }
         }
     }
@@ -71,7 +99,7 @@ namespace Scripts.Model.Characters.Behavior
         }
         public Move Filter(Move move, IHasBehavior character)
         {
-            HashSet < Direction8 > canMoveDirections = DirectionMethods.AllDirections.Where(direction => character.CanMove(direction)).ToHashSet();
+            HashSet<Direction8> canMoveDirections = DirectionMethods.AllDirections.Where(direction => character.CanMove(direction)).ToHashSet();
             _inStraightway = canMoveDirections.Count() == 2;
             if (_inStraightway)
             {

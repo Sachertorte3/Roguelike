22cfe174f76585b7fb0510c50736651cbd73e5c4
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-05 19:41:50 +0900

    feat: Display an arrow in the direction the character is facing

---
+++ Assets/Scripts/View/CharacterArrow.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks.Triggers;
+using R3;
+using Scripts.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Scripts.View
+{
+    public class CharacterArrow: MonoBehaviour
+    {
+        public void Constract(ReactiveProperty<Direction8> characterDirection)
+        {
+            characterDirection.Subscribe(direction => transform.localEulerAngles = new Vector3(0, 0, direction.Angle().Value - 90));
+        }
+    }
+}

c6b2c4988ae7762d58cf77a1bc436c5e7f69df4f
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-08 15:20:53 +0900

    refactor: Separate the responsibility for displaying sprites such as characters into a separate class

---
+++ Assets/Scripts/View/SpriteView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Scripts.View
+{
+    public class SpriteView: MonoBehaviour
+    {
+        private bool _isVisible = false;
+        public Vector3 Position() => transform.position;
+        public bool GetVisibility() => _isVisible;
+        public void SetVisibility(bool visible)
+        {
+            _isVisible = visible;
+            GetComponent<SpriteRenderer>().enabled = visible;
+        }
+    }
+}

ae5cf4f02c670f836e64ec9f95e4826313ad3a15
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-10 22:05:13 +0900

    refactor: Add an Entity class to manage Character coordinates and movement responsibilities

--- Assets/Scripts/View/CharacterView.cs
+++ Assets/Scripts/View/CharacterView.cs
@@ -1,29 +1,18 @@
 using R3;
 using Scripts.Utilities;
-using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace Scripts.View
 {
-    [RequireComponent(typeof(SpriteView), typeof(Animator))]
+    [RequireComponent(typeof(EntityView), typeof(Animator))]
     public class CharacterView : MonoBehaviour, IDirectional
     {
-        public int MoveMilliseconds = 1000;
-        public int DashMilliseconds = 1000;
-        private const int frame = 16;
-        private Func<bool> _isDash;
-        private SpriteView _view;
-        public Observable<Unit> OnMoveFinished => _onMoveFinished;
-        private Subject<Unit> _onMoveFinished = new();
         public ReadOnlyReactiveProperty<Direction8> Direction => _direction;
         private ReactiveProperty<Direction8> _direction = new();
         public Direction8 GetDirection() => Direction.CurrentValue;
-        private bool _isVisible => _view.GetVisibility();
-        public void Construct(InputReceiver receiver, string characterTypeName)
+        public void Construct(string characterTypeName)
         {
-            _isDash = () => receiver.IsDash;
-            _view = GetComponent<SpriteView>();
             RuntimeAnimatorController animation = Addressables
                 .LoadAssetAsync<RuntimeAnimatorController>($"Assets/Animations/{characterTypeName}.controller")
                 .WaitForCompletion();
@@ -32,25 +21,6 @@ namespace Scripts.View
         public void Turn(Direction8 direction)
         {
             _direction.OnNext(direction);
-        }
-        public void Move(Vector2Int destination, Direction8 direction)
-        {
-            if (_isVisible)
-            {
-                Vector3Int position = (Vector3Int)destination - (Vector3Int)direction.Vector();
-                Observable.Interval(TimeSpan.FromSeconds((_isDash() ? DashMilliseconds : MoveMilliseconds) / 1000f * 0.75f / frame))
-                .Take(frame)
-                .Index()
-                .Subscribe(l =>
-                {
-                    transform.position = Vector3.Lerp(position, (Vector3Int)destination, (l + 1) / (float)frame);
-                },
-                _ => _onMoveFinished.OnNext(Unit.Default)).AddTo(this);
-            }
-            else
-            {
-                transform.position = (Vector3Int)destination;
-            }
         }
     }
 }

4277b01d518cd91b12587369099381a290e5d809
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-11 11:43:31 +0900

    feat: Add inventory

--- Assets/Scripts/Model/Items/Item.cs
+++ Assets/Scripts/Model/Items/Item.cs
@@ -2,27 +2,19 @@
 using Scripts.Model.Action;
 using Scripts.Model.Characters.Effect;
 using Scripts.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Scripts.Model.Items
 {
-    public class Item
+    public record Item(Skill Skill)
     {
-        public Skill Skill;
-        public int UsableTimes;
-        public Item(Skill skill, int usableTimes)
-        {
-            Skill = skill;
-            UsableTimes = usableTimes;
-        }
         public async UniTask Use(IActor actor, Direction8 direction)
         {
             await Skill.Use(actor, direction);
-            UsableTimes--;
+        }
+        public float Evaluate(IActor actor, Direction8 direction)
+        {
+            return Skill.Evaluate(actor, direction);
         }
     }
+
 }

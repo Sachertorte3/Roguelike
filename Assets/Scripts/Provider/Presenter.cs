52e787c9bc8630fbbc5b482de86724d324df587f
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-05-21 16:18:04 +0900

    fix: fix some error

--- Assets/Scripts/Provider/Presenter.cs
+++ Assets/Scripts/Provider/Presenter.cs
@@ -1,11 +1,11 @@
 #nullable enable
 using Model;
 using R3;
-using Sirenix.Utilities;
 using System.Linq;
 using Unity.Logging;
 using Unity.Logging.Sinks;
 using UnityEngine;
+using Utilities;
 using Utilities.ObjectsManager;
 using VContainer;
 using View;
@@ -31,12 +31,6 @@ namespace Provider
                     .Where(view => area.AreaEntered.Contains(Vector2Int.RoundToInt(view.Position())))
                     .ForEach(view => view.SetVisibility(true));
             });
-
-            world.ActiveMap.Subscribe(mapLoaded =>
-            {
-                world.Player.Area.Refrash(world.Player.CurrentPosition);
-            });
-            world.Player.Area.Refrash(world.Player.CurrentPosition);
 
             ObjectsManager.ObserveAdd<SpriteView>().Subscribe(view =>
                 view.SetVisibility(world.Player.Area.Get()

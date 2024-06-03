da8c693847fcc764a06ecc235a165e978736bb8a
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-06-03 14:54:07 +0900

    feat: Change HP bar text color when on low HP

--- Assets/Scripts/View/DamageTextSpawner.cs
+++ Assets/Scripts/View/DamageTextSpawner.cs
@@ -10,27 +10,27 @@ namespace View
         [SerializeField] private TextMeshProUGUI damageText;
         [SerializeField] private Canvas canvas;
 
-        public void ShowDamage(Vector2Int position, int value, float percentageFromMaxHP, int textDisplayMilliseconds) =>
+        public void ShowDamage(Vector2Int position, int value, int percentageFromMaxHP, int textDisplayMilliseconds) =>
             SpawnText(position, value, percentageFromMaxHP, textDisplayMilliseconds, Color.red);
 
-        public void ShowHeal(Vector2Int position, int value, float percentageFromMaxHP, int textDisplayMilliseconds) =>
+        public void ShowHeal(Vector2Int position, int value, int percentageFromMaxHP, int textDisplayMilliseconds) =>
             SpawnText(position, value, percentageFromMaxHP, textDisplayMilliseconds, Color.green);
 
-        public void SpawnText(Vector2Int position, int value, float percentageFromMaxHP, int textDisplayMilliseconds, Color color)
+        public void SpawnText(Vector2Int position, int value, int percentageFromMaxHP, int textDisplayMilliseconds, Color color)
         {
             damageText.GetComponent<LifeTimer>().LifeTimeMilliseconds = textDisplayMilliseconds;
             TextMeshProUGUI text = Instantiate(damageText, canvas.transform);
             text.text = value.ToString();
             text.transform.position = position + new Vector2(0, 0.5f);
-            if (percentageFromMaxHP > 1)
+            if (percentageFromMaxHP > 100)
             {
                 text.fontSize = 0.6f;
             }
-            else if (percentageFromMaxHP > 0.25)
+            else if (percentageFromMaxHP > 25)
             {
                 text.fontSize = 0.5f;
             }
-            else if (percentageFromMaxHP > 0.1)
+            else if (percentageFromMaxHP > 10)
             {
                 text.fontSize = 0.4f;
             }

b491177dcc6d92b08a659313c68a2c70a672c35b
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-06-05 00:28:43 +0900

    fix: Modify the formula for calculating the influence of effects

--- Assets/Scripts/Model/Domain/Effect/HealEffect.cs
+++ Assets/Scripts/Model/Domain/Effect/HealEffect.cs
@@ -25,7 +25,7 @@ namespace Model.Domain.Effect
 
         public float Evaluate(IActorOfEffect actor, ITargetOfEffect target)
         {
-            return Mathf.Min(1, (float)Formula.Calc(actor, Power) / target.MaxHp);
+            return Mathf.Min(1, Mathf.Min(target.MaxHp - target.CurrentHp, Formula.Calc(actor, Power)) / target.MaxHp);
         }
 
         public string Info()

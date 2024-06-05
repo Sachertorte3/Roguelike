a5a1c6b7dcdc9ec2d941d39264b8ec8804ddc864
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-06-05 15:31:40 +0900

    feat: Add the ability to swap positions with non-hostile characters.

--- Assets/Scripts/Model/Domain/Action/Move.cs
+++ Assets/Scripts/Model/Domain/Action/Move.cs
@@ -1,4 +1,7 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using Model.Domain.Characters;
 using Utilities;
 
 namespace Model.Domain.Action
@@ -13,6 +16,33 @@ namespace Model.Domain.Action
         public UniTask Do(IActor actor, IMap world, IInput input)
         {
             var _ = actor.Move(Direction, input);
+            return UniTask.CompletedTask;
+        }
+
+        public float Evaluate(IActor actor, IMap world)
+        {
+            return Score;
+        }
+    }
+    internal record Swap(Direction8 Direction, float Score = 0) : IAction
+    {
+        public bool Doable(IActor actor, IMap world)
+        {
+            var target = world.GetCharactersInArea(new[] { actor.CurrentPosition + Direction.Vector() }).FirstOrDefault();
+            if (target == null)
+                return false;
+            if (target.IsEnemy(actor))
+                return false;
+            return target.CanMoveIgnoreCharacter(Direction.Reverse(), world) && actor.CanMoveIgnoreCharacter(Direction, world);
+        }
+
+        public UniTask Do(IActor actor, IMap world, IInput input)
+        {
+            var target = world.GetCharactersInArea(new[] { actor.CurrentPosition + Direction.Vector() }).FirstOrDefault();
+            if (target == null)
+                throw new InvalidOperationException("target is null");
+            var _1 = actor.Move(Direction, input);
+            var _2 = target.ForceMove(Direction.Reverse(), input);
             return UniTask.CompletedTask;
         }
 

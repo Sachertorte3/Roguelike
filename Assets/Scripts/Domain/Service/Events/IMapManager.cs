29d5710cabc0e395e03c0d31ac12957de1d4b06a
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-07-09 19:09:52 +0900

    fix: Fixed a bug in Monster House where enemy characters would overlap with the player.

--- Assets/Scripts/Domain/Service/Events/IMapManager.cs
+++ Assets/Scripts/Domain/Service/Events/IMapManager.cs
@@ -9,6 +9,7 @@ namespace Domain.Service.Events
     public interface IMapManager
     {
         public ICharacter Player { get; }
+        public HashSet<Vector2Int> GetPassablePositionsInArea(IEnumerable<Vector2Int> area);
         public HashSet<ICharacter> GetCharactersInArea(IEnumerable<Vector2Int> area);
         public HashSet<IItemEntity> GetItemsInArea(IEnumerable<Vector2Int> area);
         public IItem? GetItemFromId(Id<IItem> id);

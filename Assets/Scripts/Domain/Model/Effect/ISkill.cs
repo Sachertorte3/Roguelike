069fa735dce6391cf50d818e09f1466924b81fea
torte <nagarebosi120@gmail.com>
2024-08-31 12:01:17 +0900

    change: Improved item info

--- Assets/Scripts/Domain/Model/Effect/ISkill.cs
+++ Assets/Scripts/Domain/Model/Effect/ISkill.cs
@@ -1,25 +1,7 @@
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using Domain.Model.Action;
-using Domain.Model.Character;
-using UnityEngine;
-using Utilities;
-
 namespace Domain.Model.Effect
 {
-    public interface ISkill : IHasInfo, IHasUpgrades
+    public interface ISkill : IHasUpgrades
     {
         public float EvaluatePrice();
-    }
-    public interface ICharacterSkill : ISerializable<CharacterSkillMemento>, ISkill
-    {
-        public void UpdateTurn();
-        public bool IsUsable();
-        public Color Color { get; }
-        public int RushDistance { get; }
-        public IEnumerable<Vector2Int> GetArea(IActorOfEffect actor, Vector2Int position, Direction8 direction,
-            IEffectMap map);
-        public UniTask<bool> Use(IActor actor, Vector2Int position, Direction8 direction, IMap map);
-        public float Evaluate(IActor actor, Vector2Int position, Direction8 direction, IMap world);
     }
 }

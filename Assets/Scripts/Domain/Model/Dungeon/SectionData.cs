6c25d8ad749d79924827ceb21ad90bc592d9792d
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-10-10 20:47:46 +0900

    feat: Adding rounded corners to rooms

--- Assets/Scripts/Domain/Model/Dungeon/SectionData.cs
+++ Assets/Scripts/Domain/Model/Dungeon/SectionData.cs
@@ -13,6 +13,7 @@ namespace Domain.Model.Dungeon
     {
         public int Depth => Floors.Sum(floor => floor.Depth);
         public SectionType Type;
+        public bool RoundRoomCorner;
         [Range(0, 1)] public float WaterChance;
         [RequiredListLength(1, null)] public List<FloorData> Floors;
         private bool _existChest => Floors.Any(floor => floor.Room.ChestChance > 0);

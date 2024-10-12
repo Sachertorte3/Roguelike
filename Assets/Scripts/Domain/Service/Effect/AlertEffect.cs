2af428418e43cfa27d04b5d153adb02689ff3def
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-10-12 22:45:49 +0900

    feat: Adding random effect

--- Assets/Scripts/Domain/Service/Effect/AlertEffect.cs
+++ Assets/Scripts/Domain/Service/Effect/AlertEffect.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
+using Domain.Model;
 using Domain.Model.Effect;
 using Domain.Model.Map;
 using UnityEngine;
@@ -43,29 +45,70 @@ namespace Domain.Service.Effect
         }
     }
     [Serializable]
-    public class ForgetEffect : IActorlessEffect
+    public class RandomEffect : IEffect
     {
-        public Color Color => Colors.White;
+        [SerializeReference] public List<IEffect> Effects;
+        private int _index = -1;
+        private bool _notRandomized = true;
+
+        public Color Color => _index == -1 ? Colors.White : Effects[_index].Color;
 
         public Impact Impact => Impact.Harmful;
 
-        public UniTask Apply(IActorOfEffect actor, ITargetOfEffect target, IMap map) => Apply(target, map);
-        public UniTask Apply(ITargetOfEffect target, IMap map)
+        public UniTask Apply(IActorOfEffect actor, ITargetOfEffect target, IMap map)
         {
-            target.ClearKnownItems(map);
-            target.ClearAffiliation(map);
-
-            return UniTask.CompletedTask;
+            if (_notRandomized)
+            {
+                _index = Enumerable.Range(0, Effects.Count).GetAtRandom();
+                _notRandomized = false;
+            }
+            return Effects[_index].Apply(actor, target, map);
         }
 
+        public UniTask Apply(IActorOfEffect actor, IEntity target, IMap map)
+        {
+            if (_notRandomized)
+            {
+                _index = Enumerable.Range(0, Effects.Count).GetAtRandom();
+                _notRandomized = false;
+            }
+            return Effects[_index].Apply(actor, target, map);
+        }
+
+        public UniTask Apply(IActorOfEffect actor, IEnumerable<Vector2Int> positions, IMap map)
+        {
+            if (_notRandomized)
+            {
+                _index = Enumerable.Range(0, Effects.Count).GetAtRandom();
+                _notRandomized = false;
+            }
+            var effect = Effects[_index];
+            _notRandomized = true;
+            return effect.Apply(actor, positions, map);
+        }
+
+        private int ImpactValue(Impact impact)
+        {
+            return impact switch
+            {
+                Impact.Neutral => 0,
+                Impact.Harmful => 1,
+                Impact.Beneficial => -1,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
         public float Evaluate(IActorOfEffect actor, ITargetOfEffect target)
         {
-            return 0.2f;
+            return Effects.Average(effect => ImpactValue(effect.Impact) * effect.Evaluate(actor, target));
+        }
+        public float Evaluate(IActorOfEffect actor, IEnumerable<Vector2Int> positions)
+        {
+            return Effects.Average(effect => ImpactValue(effect.Impact) * effect.Evaluate(actor, positions));
         }
 
         public float EvaluatePrice()
         {
-            return 100;
+            return Effects.Average(effect => effect.EvaluatePrice());
         }
 
         public Dictionary<UpgradePath, UpgradeData> GetUpgrades()
@@ -75,7 +118,7 @@ namespace Domain.Service.Effect
 
         public string Info()
         {
-            return $"忘却";
+            return $"ランダム";
         }
     }
 }
---
+++ Assets/Scripts/Domain/Service/Effect/ForgetEffect.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Domain.Model.Effect;
+using Domain.Model.Map;
+using UnityEngine;
+using Utilities;
+
+namespace Domain.Service.Effect
+{
+    [Serializable]
+    public class ForgetEffect : IActorlessEffect
+    {
+        public Color Color => Colors.White;
+
+        public Impact Impact => Impact.Harmful;
+
+        public UniTask Apply(IActorOfEffect actor, ITargetOfEffect target, IMap map) => Apply(target, map);
+        public UniTask Apply(ITargetOfEffect target, IMap map)
+        {
+            target.ClearKnownItems(map);
+            target.ClearAffiliation(map);
+
+            return UniTask.CompletedTask;
+        }
+
+        public float Evaluate(IActorOfEffect actor, ITargetOfEffect target)
+        {
+            return 0.2f;
+        }
+
+        public float EvaluatePrice()
+        {
+            return 100;
+        }
+
+        public Dictionary<UpgradePath, UpgradeData> GetUpgrades()
+        {
+            return new Dictionary<UpgradePath, UpgradeData>();
+        }
+
+        public string Info()
+        {
+            return $"忘却";
+        }
+    }
+}

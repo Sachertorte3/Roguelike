559f2ec48a90915d5022aa78d46b642f421a2036
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-10-12 13:31:15 +0900

    feat: Add the Forget Effect

--- Assets/Scripts/Domain/Model/Effect/ITargetOfEffect.cs
+++ Assets/Scripts/Domain/Model/Effect/ITargetOfEffect.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Model.Character;
 using Domain.Model.Condition;
 using Domain.Model.Item;
+using Domain.Model.Map;
 using Utilities;
 
 namespace Domain.Model.Effect
@@ -38,6 +39,8 @@ namespace Domain.Model.Effect
 
         public void AddCondition(Id<IEntity> actor, IConditionData condition, RemovalConditionData removalCondition);
         public void ClearCondition();
+        public void ClearKnownItems(IMap map);
+        public void ClearAffiliation(IMap map);
         public void ListenToAlert(IActorOfEffect actor);
     }
 }

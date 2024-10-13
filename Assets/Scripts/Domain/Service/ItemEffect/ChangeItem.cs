2b5e8e9da914f74a8f2e0d699ef42c983575c49d
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-10-13 22:18:36 +0900

    feat: Add effects that change items

---
+++ Assets/Scripts/Domain/Service/ItemEffect/ChangeItem.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Model.Dungeon;
+using Domain.Model.Item;
+using Domain.Service.Items;
+using UnityEngine;
+
+namespace Domain.Service.ItemEffect
+{
+    [Serializable]
+    public class ChangeItem : IItemEffect
+    {
+        [SerializeField] private ItemData _item;
+
+        public IEnumerable<int> GetDisabledItemIndexes(IHasInventory actor)
+        {
+            return Enumerable.Empty<int>();
+        }
+
+        public void Apply(IHasInventory actor, IItem item, ItemDatabase itemDatabase)
+        {
+            actor.Inventory.Replace(new Item(_item), actor.Inventory.GetItemIndex(item));
+        }
+
+        public float EvaluatePrice()
+        {
+            return 100;
+        }
+
+        public string Info()
+        {
+            return $"変化({_item.name})";
+        }
+    }
+}

a724bbb1153b1af21af45af9ddc6502381e2a12f
Torte <93250466+Sachertorte3@users.noreply.github.com>
2024-10-21 22:04:09 +0900

    feat: Improve DashFilter

--- Assets/Scripts/Domain/Service/Characters/Behavior/IntelligentDashController.cs
+++ Assets/Scripts/Domain/Service/Characters/Behavior/IntelligentDashController.cs
@@ -5,13 +5,13 @@ using Domain.Model.Character;
 using Domain.Model.Map;
 using Domain.Model.Setting;
 using Domain.Service.Action;
-using Unity.Logging;
 using Utilities;
 
 namespace Domain.Service.Characters.Behavior
 {
     internal sealed class IntelligentDashController
     {
+        private Direction8 _lastMoveDirection;
         public async UniTask Wait(IHasBehavior character, IMap map)
         {
             if (character.CanMove(character.CurrentDirection, map) &&
@@ -26,10 +26,11 @@ namespace Domain.Service.Characters.Behavior
                 await UniTask.Delay(Settings.DashPauseMilliseconds.Value);
         }
 
-        public Move Filter(Move move, IHasBehavior character, bool started, IMap map, IInput input)
+        public Move Filter(Move move, IHasBehavior character, bool isStarted, IMap map, IInput input)
         {
             move = MoveFilter(move, character, map);
-            move = DashFilter(move, character, started, map, input);
+            move = DashFilter(move, character, isStarted, map, input);
+            _lastMoveDirection = move.Direction;
             return move;
         }
 
@@ -50,32 +51,23 @@ namespace Domain.Service.Characters.Behavior
             return move;
         }
 
-        private Move DashFilter(Move move, IHasBehavior character, bool started, IMap map, IInput input)
+        private Move DashFilter(Move move, IHasBehavior character, bool isStarted, IMap map, IInput input)
         {
-            if (!IsDashingStraight(started, input)) return move;
-            var canMoveDirections = DirectionMethods.AllDirections
-                .Where(direction => character.CanMove(direction, map))
-                .ToHashSet();
-            var inStraightway = canMoveDirections.Count() == 2;
+            if (!input.IsDash() || isStarted) return move;
+            var inStraightway = DirectionMethods.AllDirections
+                .Where(direction => !direction.IsDiagonal())
+                .Where(direction => character.CanMoveIgnoreEntity(direction, map))
+                .Count() == 2;
             if (inStraightway)
             {
-                var lastMoveDirection = character.CurrentDirection;
-                if (!canMoveDirections.Remove(lastMoveDirection.Reverse()))
-                {
-                    Log.Info(
-                        $"The possible position of the previous turn based on the character's direction({character.CurrentDirection}) is not a passage.");
-                    return move;
-                }
-
-                return new Move(canMoveDirections.First());
+                var canMoveDirections = DirectionMethods.AllDirections
+                    .Where(direction => !direction.IsDiagonal())
+                    .Where(direction => character.CanMove(direction, map))
+                    .Where(direction => direction != _lastMoveDirection.Reverse());
+                if (canMoveDirections.Count() == 1)
+                    return new Move(canMoveDirections.First());
             }
-
             return move;
-        }
-
-        public bool IsDashingStraight(bool started, IInput input)
-        {
-            return input.IsDash() && !started;
         }
     }
 }

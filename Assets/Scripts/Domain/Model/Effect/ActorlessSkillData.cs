9fb0d86621b33f03d306e5e0b4231a82ea939de9
Torte <93250466+Sachertorte3@users.noreply.github.com>
2025-10-12 15:13:57 +0900

    fix: Fixed a bug that caused traps and statues to not function properly.

--- Assets/Scripts/Domain/Model/Effect/ActorlessSkillData.cs
+++ Assets/Scripts/Domain/Model/Effect/ActorlessSkillData.cs
@@ -23,6 +23,7 @@ namespace Domain.Model.Effect
         [field: Required]
         public List<IActorlessEffect> Effects { get; private set; }
 
+        [field: SerializeField]
         [field: MinValue(1)]
         public int Repeats { get; private set; } = 1;
 
